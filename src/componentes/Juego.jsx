7b98ec0fc0e01c0a266098c50a0f2aa5a46d58d2
braguilera <braaguileraa@gmail.com>
2024-10-25 22:17:53 -0300

    Metodo para un numero aleatorio hecho

--- src/componentes/Juego.jsx
+++ src/componentes/Juego.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Contexto from '../contexto/Contexto';
 
 const Juego = () => {
-    const navegacion=useNavigate();        
+    const numeros=[0,1,2,3,4,5,6,7,8,9];
+    const navegacion=useNavigate();  
+    const {azar,setAzar}=useContext(Contexto);
+    
+    useEffect(() => {
+        const nuevosNumeros = [];
+        for (let i = 0; i < 4; i++) {
+            nuevosNumeros.push(Math.floor(Math.random() * 9));
+        }
+        setAzar(nuevosNumeros);
+    }, []);
+    
+
     return (
         <>
+            {azar}
+
+            
+
             <button className='boton_jugar' onClick={()=>navegacion(-1)}>Volver atrás</button>
         </>
     )
--- src/contexto/Provider.jsx
+++ src/contexto/Provider.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Contexto from "./Contexto"
 
 
 const Provider = ({children}) => {
+    const [azar,setAzar]=useState([]);
+
+
     return (
-        <Contexto.Provider value={{}}>
+        <Contexto.Provider value={{azar,setAzar}}>
             {children}
         </Contexto.Provider>
     )
