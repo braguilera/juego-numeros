d3a0e639a3243f0959756df93cc227b59eb88cfb
braguilera <braaguileraa@gmail.com>
2024-11-23 20:09:14 -0300

    Ruta de error te permite volver al inicio

--- src/componentes/Error.jsx
+++ src/componentes/Error.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
+
+    const navegacion = useNavigate();
     return (
     <>
     <section className='error'>
         <h1>Error: Esta página no existe!</h1>
-
+        <button className='volver_inicio' onClick={() => navegacion('/')}>Volver al inicio</button>
     </section>
     </>
     )
